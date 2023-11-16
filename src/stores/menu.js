860b7eca8d39c18545a7c293cf23426214ae926f
jinju9553 <jinju9553@gmail.com>
2023-11-16 17:10:46 +0900

    Chore: 필요없는 코드 제거

--- src/stores/menu.js
+++ src/stores/menu.js
@@ -3,21 +3,14 @@ import { defineStore } from "pinia";
 
 export const useMenuStore = defineStore("menuStore", () => {
   const menuList = ref([
-    { name: "회원가입", show: true, routeName: "regist" },
-    { name: "로그인", show: true, routeName: "login" },
-    // { name: "오늘할일", show: false, routeName: "todos" },
-    { name: "내정보", show: false, routeName: "mypage" },
-    { name: "로그아웃", show: false, routeName: "logout" },
+    { name: "회원가입", show: true, routeName: "user-join" },
+    { name: "로그인", show: true, routeName: "user-login" },
+    { name: "내정보", show: false, routeName: "user-mypage" },
+    { name: "로그아웃", show: false, routeName: "user-logout" },
   ]);
 
   const changeMenuState = () => {
-    
-    console.log("변경 전")
-    console.log(menuList.value)
     menuList.value = menuList.value.map((item) => ({ ...item, show: !item.show }));
-    
-    console.log("변경 후")
-    console.log(menuList.value)
   };
   return {
     menuList,
