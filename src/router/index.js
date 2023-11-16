51f392e73153d9fd27ae258dddfb5e9d437bf920
jinju9553 <jinju9553@gmail.com>
2023-11-16 17:10:28 +0900

    Chore: user router 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TheMainView from '@/views/TheMainView.vue'
 
+import { useMemberStore } from "@/stores/member";
+import { useMenuStore } from "@/stores/menu";
+
+const onlyAuthUser = async (to, from, next) => {
+  const memberStore = useMemberStore();
+  let token = sessionStorage.getItem("accessToken");
+  console.log(
+    "로그인 처리 전================================",
+    memberStore.userInfo,
+    token
+  );
+
+  if (memberStore.userInfo != null && token) {
+    console.log("토큰 유효성 체크하러 가자!!!!");
+    await memberStore.getUserInfo(token);
+  }
+  if (!memberStore.isValidToken || memberStore.userInfo === null) {
+    alert("로그인이 필요한 페이지입니다..");
+    router.push({ name: "user-login" });
+  } else {
+    console.log("로그인 했다!!!!!!!!!!!!!.");
+    next();
+  }
+};
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -19,20 +44,45 @@ const router = createRouter({
       // redirect: { name: "attraction" },
     },
     {
-      path: "/login",
-      name: "login",
-      component: () => import("@/components/user/UserLogin.vue")
+      path: "/user",
+      name: "user",
+      component: () => import("@/views/TheUserView.vue"),
+      children: [
+        {
+          path: "/login",
+          name: "user-login",
+          component: () => import("@/components/user/UserLogin.vue"),
+        },
+        {
+          path: "/join",
+          name: "user-join",
+          component: () => import("@/components/user/UserRegist.vue"),
+        },
+        {
+          path: "/mypage",
+          name: "user-mypage",
+          component: () => import("@/components/user/UserMyPage.vue"),
+        },
+        {
+          path: "/logout",
+          name: "user-logout",
+          beforeEnter(to, from, next) { 
+            const memberStore = useMemberStore();
+            const { changeMenuState } = useMenuStore();
+            console.log(memberStore.userInfo.userId)
+            memberStore.userLogout(memberStore.userInfo.userId);
+            changeMenuState();
+            next('/');
+          }
+        },
+        // {
+        //   path: "modify/:userid",
+        //   name: "user-modify",
+        //   component: () => import("@/components/users/UserModify.vue"),
+        // },
+      ],
     },
-    {
-      path: "/regist",
-      name: "regist",
-      component: () => import("@/components/user/UserRegist.vue")
-    },
-    {
-      path: "/mypage",
-      name: "mypage",
-      component: () => import("@/components/user/UserMyPage.vue")
-    },
+
     {
       path: "/board",
       name: "board",
