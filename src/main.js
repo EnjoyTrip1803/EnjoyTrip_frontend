e552037980fecb86bd172d056ae5b3543d1618d2
INSEA-99 <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>
2023-11-17 15:43:13 +0900

    Chore: add antd package

--- src/main.js
+++ src/main.js
@@ -7,9 +7,13 @@ import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 
+import Antd from 'ant-design-vue';
+import "ant-design-vue/dist/antd.css";
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd)
 
 app.mount('#app')
