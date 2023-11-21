f5042aff89ddcbd3b6e285da6e39e7a68a1d0511
INSEA-99 <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>
2023-11-21 11:47:46 +0900

    Feat: add main sido card view

---
+++ src/components/common/ImageCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+// defineProps({
+    //     card: Object
+    // })
+    
+import { ref } from 'vue';
+
+const card = ref({
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8sgzRZllAB6iub5Cf8B9Z0OTaLLneSb3vtw&usqp=CAU",
+    title: "여행지명",
+    info: "정보"
+
+})
+</script>
+
+<template>
+   <div class="card" >
+        <img class="card-img-top" :src="`${card.image}`" alt="Card image">
+        <div class="card-body">
+            <h4 class="card-title">{{card.title}}</h4>
+            <p class="card-text">{{card.info}}</p>
+            <a href="#" class="btn btn-primary">자세히</a>
+        </div>  
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 100%;
+    margin: 10px;
+}
+.img-wrapper {
+    position: relative;
+    width: 500px;
+    height: 500px;
+}
+.card > img {
+}
+</style>
---
+++ src/components/main/MainSidoCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+import ImageCard from "../components/common/ImageCard.vue";
+
+const list = ref([1, 2, 3, 4, 5, 6])
+</script>
+
+<template>
+   <div class="row" id="card-wrapper">
+        <div v-for="i in list" class="col-sm-5 col-xl-3 ">
+            <ImageCard card="card"/>
+        </div>
+   </div>
+</template>
+
+<style scoped>
+#card-wrapper {
+    margin: auto 10%;
+}
+</style>
