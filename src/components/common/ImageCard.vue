c70bd17c66a6b4937b837cbe3cb24ee404b3b1a7
서성원 <zsa332@gmail.com>
2023-11-21 17:55:06 +0900

    Design : 여행지 card 디자인

--- src/components/common/ImageCard.vue
+++ src/components/common/ImageCard.vue
@@ -1,39 +1,77 @@
 <script setup>
 // defineProps({
-    //     card: Object
-    // })
-    
+//     card: Object
+// })
+
 import { ref } from 'vue';
 
-const card = ref({
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8sgzRZllAB6iub5Cf8B9Z0OTaLLneSb3vtw&usqp=CAU",
-    title: "여행지명",
-    info: "정보"
-
-})
+const cards = ref([
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8sgzRZllAB6iub5Cf8B9Z0OTaLLneSb3vtw&usqp=CAU",
+        title: "여행지명",
+        info: "정보"
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8sgzRZllAB6iub5Cf8B9Z0OTaLLneSb3vtw&usqp=CAU",
+        title: "여행지명",
+        info: "정보"
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8sgzRZllAB6iub5Cf8B9Z0OTaLLneSb3vtw&usqp=CAU",
+        title: "여행지명",
+        info: "정보"
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8sgzRZllAB6iub5Cf8B9Z0OTaLLneSb3vtw&usqp=CAU",
+        title: "여행지명",
+        info: "정보"
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8sgzRZllAB6iub5Cf8B9Z0OTaLLneSb3vtw&usqp=CAU",
+        title: "여행지명",
+        info: "정보"
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8sgzRZllAB6iub5Cf8B9Z0OTaLLneSb3vtw&usqp=CAU",
+        title: "여행지명",
+        info: "정보"
+    },
+])
 </script>
 
 <template>
-   <div class="card" >
-        <img class="card-img-top" :src="`${card.image}`" alt="Card image">
-        <div class="card-body">
-            <h4 class="card-title">{{card.title}}</h4>
-            <p class="card-text">{{card.info}}</p>
-            <a href="#" class="btn btn-primary">자세히</a>
-        </div>  
+    <div class="continer">
+        <div class="row row-cols-3 m-5">
+            <div class="card" style="width: 18rem;" v>
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                        card's content.</p>
+                </div>
+            </div>
+            <div class="card col" v-for="card in cards">
+                <img class="card-img-top" :src="`${card.image}`" alt="Card image">
+                <div class="card-body">
+                    <h4 class="card-title">{{ card.title }}</h4>
+                    <p class="card-text">{{ card.info }}</p>
+                    <a href="#" class="btn btn-primary">자세히</a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-.card {
+/* .card {
     width: 100%;
     margin: 10px;
 }
+
 .img-wrapper {
     position: relative;
     width: 500px;
     height: 500px;
-}
-.card > img {
-}
+} */
+
+.card>img {}
 </style>
--- src/views/TheMainView.vue
+++ src/views/TheMainView.vue
@@ -1,34 +1,38 @@
 <script setup>
 import TheHeadingNavbar from "@/components/layout/TheHeadingNavbar.vue"
+import ImageCard from '@/components/common/ImageCard.vue';
 </script>
 
 <template>
     <div class="masthead">
-        <TheHeadingNavbar :transparent="true"/>
+        <TheHeadingNavbar :transparent="true" />
         <div class="color-overlay d-flex justify-content-center align-items-center">
             <h1>Put It All Down For A While</h1>
         </div>
     </div>
+    <div class="m-10">
+        <ImageCard></ImageCard>
+    </div>
 </template>
 
 <style scoped>
-    .masthead {
-        background-image: url('@/assets/img/banners/driving.png');
-        height: 100vh;
-        min-height: 400px;
-        background-size: cover;
-        position: relative;
-        color: white;
-        text-shadow: 2px 2px 2px rgba(0,0,0,.2);
-    }
+.masthead {
+    background-image: url('@/assets/img/banners/driving.png');
+    height: 100vh;
+    min-height: 400px;
+    background-size: cover;
+    position: relative;
+    color: white;
+    text-shadow: 2px 2px 2px rgba(0, 0, 0, .2);
+}
 
-    h1{
-        color:white;
-    }
+h1 {
+    color: white;
+}
 
-    .color-overlay{
-        position: absolute;
-        width: 100%;
-        height:100%;
-    }
+.color-overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+}
 </style>
