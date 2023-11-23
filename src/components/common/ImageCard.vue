ee373ace327099335ed6452f3842d28e84562198
서성원 <zsa332@gmail.com>
2023-11-23 17:43:39 +0900

    Design: img 사이즈 설정

--- src/components/common/ImageCard.vue
+++ src/components/common/ImageCard.vue
@@ -9,12 +9,11 @@ const onPlan = () => {
 </script>
 
 <template>
-    <div class="card">
+    <div class="card" @click="onPlan">
         <img class="card-img-top" :src="props.card.image" alt="Card image">
         <div class="card-body">
             <h4 class="card-title">{{ props.card.title }}</h4>
             <p class="card-text">{{ props.card.info }}</p>
-            <button class="btn btn-primary" @click="onPlan">계획하기</button>
         </div>
     </div>
 </template>
@@ -29,6 +28,12 @@ const onPlan = () => {
     position: relative;
     width: 500px;
     height: 500px;
+}
+
+.card-img-top {
+    width: 100%;
+    height: 260px;
+    object-fit: cover;
 }
 
 .card>img {}
